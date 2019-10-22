6cf44769c3fd6c50dc04a0733afeb5addaf2dbcf
RomanTsisyk <romantsisyk1@gmail.com>
2019-10-22 13:07:37 +0200

    Innit commit

---
+++ app/src/main/java/tsisyk/app/rssfeed/MainActivity.kt
@@ -0,0 +1,12 @@
+package tsisyk.app.rssfeed
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
