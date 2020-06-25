27d4bfb56802eda43179a8a36900eb9a68363449
romantsisyk <romantsisyk1@gmail.com>
2020-06-25 06:30:39 +0200

    Create Readme and some gifs

---
+++ README.md
@@ -0,0 +1,4 @@
+# Kitty & MVVM + TDD
+
+
+<img src="https://github.com/RomanTsisyk/Kitty_rank/blob/master/new_kitty"><img src="https://github.com/RomanTsisyk/Kitty_rank/blob/master/recycle_vew">
--- app/src/main/java/tsisyk/app/kittyrank/model/AttributeStore.kt
+++ app/src/main/java/tsisyk/app/kittyrank/model/AttributeStore.kt
@@ -3,6 +3,7 @@ package tsisyk.app.kittyrank.model
 object AttributeStore {
     val MY_OPINION: List<AttributeValue> by lazy {
         val myOpinion = mutableListOf<AttributeValue>()
+        myOpinion.add(AttributeValue("Do you like her?", 0))
         myOpinion.add(AttributeValue("don`t like", 1))
         myOpinion.add(AttributeValue("don`t know", 2))
         myOpinion.add(AttributeValue("like", 3))
@@ -11,15 +12,17 @@ object AttributeStore {
     }
     val KITTIES_OPINION: List<AttributeValue> by lazy {
         val kitty_opinion = mutableListOf<AttributeValue>()
-        kitty_opinion.add(AttributeValue("don`t lokis me" , -1))
-        kitty_opinion.add(AttributeValue("don`t know", 0))
-        kitty_opinion.add(AttributeValue("likes me", 1))
-        kitty_opinion.add(AttributeValue("loves me", 2))
+        kitty_opinion.add(AttributeValue("Does she like you?" , 0))
+        kitty_opinion.add(AttributeValue("don`t lokis me" , 1))
+        kitty_opinion.add(AttributeValue("don`t know", 1))
+        kitty_opinion.add(AttributeValue("likes me", 4))
+        kitty_opinion.add(AttributeValue("loves me", 7))
         kitty_opinion
     }
     val EMOTIONS: List<AttributeValue> by lazy {
         val emotions = mutableListOf<AttributeValue>()
-        emotions.add(AttributeValue("Angry",0))
+        emotions.add(AttributeValue("Is she kind?",0))
+        emotions.add(AttributeValue("Angry",-1))
         emotions.add(AttributeValue("Sad", 1))
         emotions.add(AttributeValue("Normal", 2))
         emotions.add(AttributeValue("Funny", 3))
@@ -29,9 +32,10 @@ object AttributeStore {
 
     val AGE: List<AttributeValue> by lazy {
         val age = mutableListOf<AttributeValue>()
-        age.add(AttributeValue("To young",0))
-        age.add(AttributeValue("ok", 1))
-        age.add(AttributeValue("To old", -1))
+        age.add(AttributeValue("How old is she?",0))
+        age.add(AttributeValue("To young",2))
+        age.add(AttributeValue("ok", 8))
+        age.add(AttributeValue("To old", 3))
 
         age
     }
