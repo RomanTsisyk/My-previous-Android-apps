78d66c95bb35577a49b571bb810085a38c968de0
romantsisyk <romantsisyk1@gmail.com>
2020-08-04 17:25:54 +0200

    Create SplashActivity

--- app/src/main/java/app/tsisyk/empikweather/ui/MainActivity.kt
+++ app/src/main/java/app/tsisyk/empikweather/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package app.tsisyk.empikweather
+package app.tsisyk.empikweather.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import app.tsisyk.empikweather.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
